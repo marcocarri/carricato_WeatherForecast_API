d6b5e408e07a7df716e30b3a4d8328b2de2f1782
marcocarri <carricatomarco@gmail.com>
2026-06-05 18:46:27 +0200

    implementazione di weather/urls.py e modifica a config/urls.py implementazione del corretto funzionamento degli urls per fare richieste alla nostra API

---
+++ weather/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import WeatherForecastView, SavedQueryViewSet
+
+# il DefaultRouter gestisce automaticamente tutti gli URL per le operazioni CRUD del ViewSet
+router = DefaultRouter()
+router.register(r'saved', SavedQueryViewSet, basename='saved-query')
+
+urlpatterns = [
+    path('forecast/', WeatherForecastView.as_view(), name='weather-forecast'),
+    path('', include(router.urls)),
+]
