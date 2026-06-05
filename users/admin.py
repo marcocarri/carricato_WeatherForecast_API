ba650e8dd4710bc6a16b8c4e1d761977fe505211
marcocarri <carricatomarco@gmail.com>
2026-06-05 18:14:10 +0200

    implementazione dei modelli in users/admin.py implementazione del modello User custom nel panello del admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# estendiamo i campi mostrati nel pannello admin per includere 'is_premium'
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Ruoli Personalizzati', {'fields': ('is_premium',)}),
+    )
+
+# registriamo il modello CustomUser nel pannello Admin
+admin.site.register(CustomUser, CustomUserAdmin)
