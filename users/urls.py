4d1bf26863229fab6ef28382a6718b9fb109b8e8
marcocarri <carricatomarco@gmail.com>
2026-06-10 18:41:26 +0200

    modifica users/urls.py modifica per il corretto funzionamento degli urls per fare richieste alla nostra API

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, UpgradePremiumView, DowngradePremiumView
 
 urlpatterns = [
     # endpoint login (crea il JWT)
@@ -9,4 +9,8 @@ urlpatterns = [
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # endpoint registrazione
     path('register/', RegisterView.as_view(), name='register'),
+    # endpoint upgrade account
+    path('upgrade/', UpgradePremiumView.as_view(), name='upgrade_premium'),
+    # endpoint downgrade account
+    path('downgrade/', DowngradePremiumView.as_view(), name='downgrade_premium'),
 ]
