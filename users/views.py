7efbe52695c3c7e220d22706d61637fb42726fd3
marcocarri <carricatomarco@gmail.com>
2026-06-10 18:17:20 +0200

    implementazione di users/serializers.py e users/views.py implementazione di modelli e funzioni che permettono la registrazione di un utente e il login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = UserRegistrationSerializer
