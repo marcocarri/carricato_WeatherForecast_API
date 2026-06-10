7b36e716a3ceed92cc7dd5c801824f02e751c1a4
marcocarri <carricatomarco@gmail.com>
2026-06-10 18:39:17 +0200

    modifica users/views.py implementazione degli endpoint per effettuare upgrade/downgrade dell'account

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import get_user_model
 from .serializers import UserRegistrationSerializer
 
@@ -9,3 +11,44 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = UserRegistrationSerializer
+
+
+class UpgradePremiumView(APIView):
+    """
+    endpoint per simulare l'acquisto dell'abbonamento Premium
+    richiede autenticazione (JWT)
+    """
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        if user.is_premium:
+            return Response({"messaggio": "Sei già un utente Premium!"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Effettuiamo l'upgrade!
+        user.is_premium = True
+        user.save()
+
+        return Response({
+            "messaggio": "Congratulazioni! Ora sei un utente Premium. Hai accesso alle ricerche meteo illimitate e al salvataggio dello storico."
+        }, status=status.HTTP_200_OK)
+
+
+class DowngradePremiumView(APIView):
+    """
+    endpoint per annullare l'abbonamento Premium e tornare Standard.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        if not user.is_premium:
+            return Response({"messaggio": "Il tuo account è già Standard."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Effettuiamo il downgrade
+        user.is_premium = False
+        user.save()
+
+        return Response({
+            "messaggio": "Hai disdetto l'abbonamento. Ora sei un utente Standard."
+        }, status=status.HTTP_200_OK)
